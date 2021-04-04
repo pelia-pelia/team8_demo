580be6480d9add213b22b59612453956cf5ef52a
pelia-pelia <77154978+pelia-pelia@users.noreply.github.com>
2021-04-04 06:47:19 +0000

    Deploying to gh-pages from @ pelia-pelia/team8_demo@fcc92d6e999fedade7a464bce9b1ad15b00d659b 🚀

--- src.64350aa8.js
+++ src.64350aa8.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team8_demo/src.99a6f3a2.js.map
+//# sourceMappingURL=/team8_demo/src.64350aa8.js.map
